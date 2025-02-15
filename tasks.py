d8087df319eae6cef899b3192cd497a40bc62546
Kselami <selami-karim@hotmail.com>
2025-02-15 10:51:34 +0100

    Ajout de la fonction de suppression de tâches dans main

--- tasks.py
+++ tasks.py
@@ -1,2 +1,3 @@
 def create_task(title, description):\n     TODO: Implémenter la création de tâches\n    pass
 def update_task(task_id, title, description):\n     TODO: Implémenter la mise à jour de tâches\n    pass
+def delete_task(task_id):\n    TODO: Implémenter la suppression de tâches\n    pass
