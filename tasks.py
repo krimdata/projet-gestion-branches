3f345045cfce246f488a402737d19c655c08222b
Kselami <selami-karim@hotmail.com>
2025-02-15 10:48:13 +0100

    Ajout de la fonction de mise à jour de tâches dans feature/tasks

--- tasks.py
+++ tasks.py
@@ -1,1 +1,2 @@
 def create_task(title, description):\n     TODO: Implémenter la création de tâches\n    pass
+def update_task(task_id, title, description):\n     TODO: Implémenter la mise à jour de tâches\n    pass
